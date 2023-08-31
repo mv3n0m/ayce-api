fd5f661783dc04a86649e679fa3953664501abad
Manish Roy <manish.11612939@lpu.in>
2023-09-01 00:06:44 +0530

    added decorators to handle routes and authentication

---
+++ src/decorators/__init__.py
@@ -0,0 +1,38 @@
+from src.docs import get_docs
+from .helpers import handle_jwt
+from src.utils import responsify
+
+
+def use_auth(_auth="jwt"):
+    def decorator(func):
+        def _wrapper(_auth=_auth, *args, **kwargs):
+            if _auth and _auth.endswith("jwt"):
+                response = handle_jwt(_auth=="_jwt")
+                if "error" in response[0]:
+                    return responsify(*response)
+
+                _kwargs, _user = response
+                args = (_user, *args)
+                kwargs.update(_kwargs)
+
+            return func(*args, **kwargs)
+
+        _wrapper.__name__ = func.__name__
+        return _wrapper
+    return decorator
+
+
+def route_wrapper(bp, use_kwargs, route, methods=["GET"], _args={}, _auth="jwt", *args, **kwargs):
+    def decorator(func):
+        @bp.route(route, methods=methods)
+        @use_auth(_auth)
+        @use_kwargs(_args)
+        def _wrapper(*inner_args, **inner_kwargs):
+            return func(*inner_args, **inner_kwargs)
+
+        _wrapper.__doc__ = get_docs(route, _args, _auth)
+        # print(_wrapper.__doc__)
+        _wrapper.__name__ = func.__name__
+        return _wrapper
+    return decorator
+
---
+++ src/decorators/helpers.py
@@ -0,0 +1,55 @@
+import jwt
+from flask import request
+from datetime import datetime
+from src.settings import rst
+from src.routes.handlers.user import User
+from src.utils import responsify, decode_access_token
+
+
+def handle_jwt(require_user=False):
+    bearer_token = request.headers.get("Authorization")
+    if not bearer_token:
+        return {"error": "JWT Token not provided."}, 400
+
+    try:
+        token = bearer_token.split("Bearer ")[1]
+        payload = decode_access_token(token)
+    except Exception as e:  # pylint: disable=bare-except
+        print(e)
+        return {"error": "Invalid JWT token."}, 400
+
+    if payload.get("exp") < datetime.now().timestamp():
+        return {"error": "JWT token expired."}, 403
+
+    user_id = payload.get("_pk")
+    _user = 'None'
+
+    try:
+        _user = rst._get(user_id)
+    except Exception as e:
+        print(e)
+
+    if _user == 'None' or require_user:
+        try:
+            user = User.from_id(user_id)
+            status = 'active' if user.active == 1 else 'inactive'
+        except Exception as e:
+            print(e)
+            return {"error": "JWT Token not associated with any user."}, 401
+
+        rst._set(user_id, status)
+        _user = status
+
+    if _user != 'active':
+        return {"error": "User inactive."}, 401
+
+    _kwargs = {}
+    try:
+        if request.method == "GET":
+            _kwargs = request.args
+        else:
+            _kwargs = request.json
+    except Exception as e:
+        print(e)
+
+    return _kwargs, user if require_user else user_id
