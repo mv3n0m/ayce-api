ffb2780c3f5c34e93f830c6ff7a78d2cde4a3bb7
Manish Roy <manish.11612939@lpu.in>
2023-12-15 00:02:03 +0530

    updated routes __init__ to accomodate all blueprints

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,5 +1,15 @@
+from .misc import bp as misc_bp
+from .users import bp as users_bp
+from .profile import bp as profile_bp
+from .settings import bp as settings_bp
+from .developers import bp as developers_bp
 from .transactions import bp as transactions_bp
 
 blueprints = [
+    misc_bp,
+    users_bp,
+    profile_bp,
+    settings_bp,
+    developers_bp,
     transactions_bp,
 ]
