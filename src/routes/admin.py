ed5cfe78ee5405a32f01f8aa6404d4e1df7ec5ad
Manish Roy <i.manish.roy@gmail.com>
2025-01-12 07:20:54 +0530

    added query params update in admin stats

--- src/routes/admin.py
+++ src/routes/admin.py
@@ -4,6 +4,7 @@ from src.utils import responsify
 from src.utils.args_schema import token_args, auth_args, email_args, password_args
 from src.settings import mdb, btcdc
 from datetime import datetime, timedelta
+from flask import request
 
 bp, route = create_blueprint("admin", __name__, "/admin")
 
@@ -20,39 +21,53 @@ def stats(_user):
     if (_user.account_type != "admin"):
         return responsify({"error": "Unauthorized"}, 401)
 
+    currency = request.args.get("currency", "usd").lower()
+    if (currency not in ["usd", "btc"]):
+        return responsify({"error": f"Invalid currency. Available options: {["usd", "btc"]}"}, 400)
+
     one_year_ago = datetime.now() - timedelta(days=366)
     unix_timestamp_one_year_ago = int(one_year_ago.timestamp())
 
     one_week_ago = datetime.now() - timedelta(days=7)
     unix_timestamp_one_week_ago = int(one_week_ago.timestamp())
 
+    one_month_ago = datetime.now() - timedelta(days=30)
+    unix_timestamp_one_month_ago = int(one_month_ago.timestamp())    
+
     one_day_ago = datetime.now() - timedelta(hours=24)
     unix_timestamp_one_day_ago = int(one_day_ago.timestamp())
 
-    # Providing data from inception for demo, need to restrict till a year
-    # data = mdb.get("transactions", {"status": "confirmed", "updated_at": {'$exists': True, '$gt': unix_timestamp_one_year_ago}}, projection={"btc_amount": 1, "usd_amount": 1, "updated_at": 1, "status": 1})
-    transactions = mdb.get("transactions", {"status": "confirmed", 'updated_at': {'$exists': True, "$type": 'int'}, 'usd_amount': {'$exists': 1}})
+    transactions = mdb.get("transactions", {"status": "confirmed", "initiated_at": {'$exists': True, '$gt': unix_timestamp_one_year_ago}, 'usd_amount': {'$exists': 1}})
     accounts = mdb.get("users", {"account_type": {"$ne": "admin"}})
 
-    per_day_transactions = list(filter(lambda _transaction: _transaction["updated_at"] > unix_timestamp_one_day_ago, transactions))
-    per_week_transactions = list(filter(lambda _transaction: _transaction["updated_at"] > unix_timestamp_one_week_ago, transactions))
+    per_day_transactions = list(filter(lambda _transaction: _transaction["initiated_at"] > unix_timestamp_one_day_ago, transactions))
+    per_week_transactions = list(filter(lambda _transaction: _transaction["initiated_at"] > unix_timestamp_one_week_ago, transactions))
+    per_month_transactions = list(filter(lambda _transaction: _transaction["initiated_at"] > unix_timestamp_one_month_ago, transactions))
+
+
+    _transactions =  {
+        "daily": sum(map(lambda _transaction: _transaction["usd_amount"], per_day_transactions)),
+        "weekly": sum(map(lambda _transaction: _transaction["usd_amount"], per_week_transactions)),
+        "montly": sum(map(lambda _transaction: _transaction["usd_amount"], per_month_transactions)),
+        "yearly": sum(map(lambda _transaction: _transaction["usd_amount"], transactions)),
+    }
+
+    _fees ={
+        "daily": sum(map(lambda _transaction:  _transaction.get("transaction_fee", 0), per_day_transactions)),
+        "weekly": sum(map(lambda _transaction:  _transaction.get("transaction_fee", 0), per_week_transactions)),
+        "monthly": sum(map(lambda _transaction:  _transaction.get("transaction_fee", 0), per_month_transactions)),
+        "yearly": sum(map(lambda _transaction:  _transaction.get("transaction_fee", 0), transactions)),
+    }
 
     data = {
-        "transactions": {
-            "day": sum(map(lambda _transaction: _transaction["usd_amount"], per_day_transactions)),
-            "week": sum(map(lambda _transaction: _transaction["usd_amount"], per_week_transactions)),
-            "year": sum(map(lambda _transaction: _transaction["usd_amount"], transactions)),
-        },
-        "fees": {
-            "day": btcdc.convert(sum(map(lambda _transaction:  _transaction.get("transaction_fee", 0), per_day_transactions)), "btc")["_to"]["amount"],
-            "week": btcdc.convert(sum(map(lambda _transaction:  _transaction.get("transaction_fee", 0), per_week_transactions)), "btc")["_to"]["amount"],
-            "year": btcdc.convert(sum(map(lambda _transaction:  _transaction.get("transaction_fee", 0), transactions)), "btc")["_to"]["amount"],
-        },
+        "transactions": {k: v if currency == 'usd' else btcdc.convert(v, "usd")["_to"]["amount"] for k, v in _transactions.items()},
+        "fees": {k: v if currency == 'btc' else btcdc.convert(v, "btc")["_to"]["amount"] for k, v in _fees.items()},
         "accounts": {
             "total": len(accounts),
             "active": len(list(filter(lambda _account: _account.get('active') == 1, accounts))),
             "inactive": len(accounts) - len(list(filter(lambda _account: _account.get('active') == 1, accounts))),
-            "locked": 0
+            "locked": 0,
+            "new": len(list(filter(lambda _account: _account.get('created_at') > unix_timestamp_one_day_ago, accounts)))
         }
     }
 
